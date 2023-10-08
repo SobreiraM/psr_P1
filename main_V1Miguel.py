12729f7f4aba13524c09d72256218023b9160bce
Miguel Sobreira <115593429+SobreiraM@users.noreply.github.com>
2023-10-08 11:57:57 +0100

    Update main_V1Miguel.py

--- main_V1Miguel.py
+++ main_V1Miguel.py
@@ -7,6 +7,7 @@
 import random
 import readchar
 from time import time, ctime
+from datetime import datetime
 import sys
 from pprint import pprint
 import argparse
@@ -15,111 +16,100 @@ from math import sqrt
 import colorama 
 from colorama import Fore, Back, Style
 
-
-############## Variáveis Globais ##############
-hit = [] 
-miss = []
-time_hit = []
-time_miss = []
-
-#def Statistics(): #vai receber parametros e calcular estatisticas pedidas para dps enviar
-    
-def Print_End(inputs, stats_dict): #será enviado um dicionário (criado na função de estatiticas) com os elementos, da seguinte forma -> stats_dict = {'accuracy': x, ....}
-                                  #onde depois serão inseridos no dicionario data da seguinte forma -> data = {'accuracy': stats_dict['accuracy']}
-    data = {'accuracy': 0, 'inputs': inputs, 'number_of_hits': 0, 'number_of_types': 0, 'test_duration': 0, 'test_end' : 0, 'test_start': 0,
-            'type_average_duration' : 0, 'type_hit_average_duration' : 0, 'type_miss_average_duration':0}
-    
-    pprint(data)
-    
-    """ Relacionado com testes da função Print_End - REMOVER DEPOIS
-    Input = namedtuple('Input', ['requested', 'received', 'duration'])
-    I1 = Input('a','b','time')
-    I2 = Input('c','d','time')
-    I3 = Input('f','e','time')
-    inputs = []
-    inputs.append(I1)
-    inputs.append(I2)
-    inputs.append(I3)
-    
-    data = {'accuracy': 0, 'inputs': inputs, 'number_of_hits': 0, 'number_of_types': 0, 'test_duration': 0, 'test_end' : 0, 'teste_start': 0,
-            'type_average_duration' : 0, 'type_hit_average_duration' : 0, 'type_miss_average_duration':0}
-    
-    
-    print('dictionary')
-    pprint(data)
-    """
-   
-"""
-def  Tempo_Palavras(segundos):
-        t = ctime()
-        t_start = time()
-        duration = t_start + segundos
+def Statistics(inputs, t_start, t_end):
         
-        hit = [] # talvez tenham que ser vars globais?
-        time_hit = []
-        miss = []
-        time_miss = []
+        n_hits = 0 
+        thad = 0
+        tmad = 0
+        duration = t_end - t_start 
         
+        for input in inputs:
+                if input[0] == input[1]: # caso requested == received
+                        n_hits += 1
+                        thad += input[2]
+                else:
+                        tmad += input[2]
         
-        while time() < duration :
+        accuracy = n_hits/len(inputs) * 100
+           
+        tad = (thad + tmad) / len(inputs)  
         
-        FALTA PREENCHER AQUI COM O CÓDIGO QUE GERA PALAVRAS
+        if n_hits == len(inputs): #evitar divisões por 0 caso utilizador acerte tudo
+                tmad = 0
+        else:
+                tmad = tmad / (len(inputs) - n_hits)
+                
+        if n_hits == 0: #evitar divisões por 0 caso utilizador erre tudo
+                thad = 0
+        else:
+                thad = thad / n_hits
         
-        t_end = time()
-        print('Current test duration (' + Fore.CYAN + (str(t_end - t_start)) + Style.RESET_ALL + ") exceeds maximum of " + str(segundos))
+        start = datetime.fromtimestamp(t_start).strftime("%A, %B %d, %Y %I:%M:%S") # parece complicado mas faz o que time.ctime() deveria fazer mas não estava a funcionar de outra forma
+        end = datetime.fromtimestamp(t_end).strftime("%A, %B %d, %Y %I:%M:%S")     # aplica o formato de data tendo o numero de segundos
+        
+        data = {'accuracy': accuracy, 'inputs': inputs, 'number_of_hits': n_hits, 'number_of_types': len(inputs), 'test_duration': duration, 'test_end' : end, 'test_start': start,
+                'type_average_duration' : tad, 'type_hit_average_duration' : thad, 'type_miss_average_duration':tmad}
+    
+        pprint(data)
         sys.exit()
-"""                
-#def  Inputs_Palavras(): 
-
         
 def  Tempo_Letras(segundos):
-        t = ctime()
+        Input = namedtuple('Input', ['requested', 'received', 'duration'])
+        inputs = [] #lista para namedtuple
+        #t = ctime() penso que isto não é preciso
         t_start = time()
         duration = t_start + segundos        
         
         while time() < duration :
                 num = random.randint(97,122)
                 key_requested = chr(num)
+                
                 print('Type letter ' + Fore.BLUE + key_requested + Style.RESET_ALL)
+                
+                t_request = time()               #usado para calcular a duração de cada input
                 key_pressed = readchar.readkey()
+                t_deliver = time()         #usado para calcular a duração de cada input                                      
+                inputs.append(Input(key_requested, key_pressed, t_deliver - t_request))
                 
                 if key_pressed == key_requested:
-                        print('You typed letter ' + Fore.GREEN + key_pressed + Style.RESET_ALL)
-                        hit.append(key_pressed) #nao pode ser feito assim, tem q se usar namedtuple... ver linha 35-42
-                        time_hit.append(time()) #nao pode ser feito assim, tem q se usar namedtuple... ver linha 35-42
+                        print('You typed letter ' + Fore.GREEN + key_pressed + Style.RESET_ALL)  
                 else:
                         print('You typed letter ' + Fore.RED + key_pressed + Style.RESET_ALL)
-                        miss.append(key_pressed)
-                        time_miss.append(time())
-        
+                        
         t_end = time()
         print('Current test duration (' + Fore.CYAN + (str(t_end - t_start)) + Style.RESET_ALL + ") exceeds maximum of " + str(segundos))
-        sys.exit()
+        
+        Statistics(inputs, t_start, t_end)
                 
 def  Inputs_Letras(max_letras):
+        Input = namedtuple('Input', ['requested', 'received', 'duration'])
+        inputs = []
+        #t = ctime()
+        t_start = time()
         n_letras = 0
         
         while n_letras < max_letras :
                 num = random.randint(97,122)
                 key_requested = chr(num)
                 print('Type letter ' + Fore.BLUE + key_requested + Style.RESET_ALL)
+                t_request = time()               #usado para calcular a duração de cada input
                 key_pressed = readchar.readkey()
+                t_deliver = time()         #usado para calcular a duração de cada input                                      
+                inputs.append(Input(key_requested, key_pressed, t_deliver - t_request))
                 
                 if key_pressed == key_requested:
                         print('You typed letter ' + Fore.GREEN + key_pressed + Style.RESET_ALL)
-                        hit.append(key_pressed) #nao pode ser feito assim, tem q se usar namedtuple... ver linha 35-42
-                        time_hit.append(time()) #nao pode ser feito assim, tem q se usar namedtuple... ver linha 35-42
                 else:
                         print('You typed letter ' + Fore.RED + key_pressed + Style.RESET_ALL)
-                        miss.append(key_pressed) #nao pode ser feito assim, tem q se usar namedtuple... ver linha 35-42
-                        time_miss.append(time()) #nao pode ser feito assim, tem q se usar namedtuple... ver linha 35-42
                 
                 n_letras += 1
         
+        t_end = time()
+        
         print(str(n_letras) + ' inputs submitted when the maximum was ' + str(max_letras))
-        sys.exit()                
+        Statistics(inputs, t_start, t_end)
                 
-                                
+                                   
 
 def main():
     
@@ -159,12 +149,7 @@ def main():
                         Inputs_Letras(args['maximum_value'])
                         
                 else:
-                        sys.exit("Bad inputs")     
-                        
-                
-               
-        
-        
+                        sys.exit("Bad inputs")        
 
 if __name__ == '__main__':
     main()
